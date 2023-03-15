4b571e8a5f65bcf655a70e8986835a2d4be8f34d
lssdg <lssdannygiftson@gmail.com>
2023-03-16 03:31:32 +0530

    verifyemail

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom"
 
 const Home = () => {
+
+    const verifyEmail = async () =>{
+        const id = localStorage.getItem("userCurr");
+        const res = await fetch('',{
+            method:'POST',
+            body:JSON.stringify({
+                requestType:'VERIFY_EMAIL',
+                idToken:id
+            })
+
+        });
+        if(res.ok){
+            const data=res.json();
+
+        }else{
+            console.log("error");
+        }
+    }
     return(<div>
         
         <header>
         <h2>Welcome to the ExpenseTracker App</h2>
         <Link to='/profile'>Complete your profile now</Link>
         </header>
+
+        <button onClick={verifyEmail}>Verify your email</button>
     </div>)
 }
 
